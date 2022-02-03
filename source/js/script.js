5c4a510e4ce93afcc85166636539c2eeeff77387
Anna Popova <panasenko-anna.8@mail.ru>
2022-02-03 14:53:31 +0300

    делает работоспособное меню при включенном jsменю работоспособным

--- source/js/script.js
+++ source/js/script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // - класс 'page-header--js-enabled' элементу page-header
   // - класс 'main-nav__list--js-enabled' элементу main-nav
   // - класс 'page-header__wrapper--js-enabled' элементу page-header-wrapper
+  // - класс 'main-nav__toggle--js-enabled' элементу nav-btn (показать гамбургер)
   //
   const listItems = document.getElementsByClassName("site-list__item");
   for (const listItem of listItems) {
@@ -15,25 +16,31 @@ document.addEventListener('DOMContentLoaded', function (event) {
   document.getElementById("page-header").classList.add('page-header--js-enabled');
   document.getElementById("main-nav").classList.add('main-nav__list--js-enabled');
   document.getElementById("page-header-wrapper").classList.add('page-header__wrapper--js-enabled');
+  document.getElementById("nav-btn").classList.add('main-nav__toggle--js-enabled');
+
+  //
+  // Спрятать открытое меню
+  //
+  document.getElementById("main-nav").classList.remove('main-nav__list--open');
+
 
   var isOpen = false;
   var button = document.getElementById('nav-btn');
   var headerWrapBlock = document.getElementById('page-header-wrapper');
   var navBlock = document.getElementById('main-nav');
 
-  //закоментированно, чтобы удовлетворить критерию Б30. Мобильное меню должно быть работоспособным при отключённом JavaScript.
-  // button.addEventListener('click', function (e) {
-  //   isOpen = !isOpen;
-  //   if (isOpen) {
-  //     headerWrapBlock.classList.add('page-header__wrapper--bg');
-  //     navBlock.classList.add('main-nav__list--open');
-  //     button.classList.add('main-nav__toggle--close');
-  //   } else {
-  //     headerWrapBlock.classList.remove('page-header__wrapper--bg');
-  //     navBlock.classList.remove('main-nav__list--open');
-  //     button.classList.remove('main-nav__toggle--close');
-  //   }
-  // }, false);
+  button.addEventListener('click', function (e) {
+    isOpen = !isOpen;
+    if (isOpen) {
+      headerWrapBlock.classList.add('page-header__wrapper--bg');
+      navBlock.classList.add('main-nav__list--open');
+      button.classList.add('main-nav__toggle--close');
+    } else {
+      headerWrapBlock.classList.remove('page-header__wrapper--bg');
+      navBlock.classList.remove('main-nav__list--open');
+      button.classList.remove('main-nav__toggle--close');
+    }
+  }, false);
 
   var formButton = document.getElementById('form-butto');
   var lastName = document.getElementById('last-name');
@@ -42,8 +49,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
   formButton.addEventListener('click', function (e) {
     alert('Работает');
-  }
-  )
+  });
 
   function updateButtonAccess() {
     if (!!lastName.value && !!name.value && !!email.value) {
