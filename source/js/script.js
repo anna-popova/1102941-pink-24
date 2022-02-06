14864efdd0bc616fe80355a82b7ed7d24e3dfdc9
Anna Popova <panasenko-anna.8@mail.ru>
2022-02-06 12:29:34 +0300

    добавляет комментарии для классов, которые переключают состояния меню и внешний вид кнопки

--- source/js/script.js
+++ source/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // - класс 'page-header--js-enabled' элементу page-header
   // - класс 'main-nav__list--js-enabled' элементу main-nav
   // - класс 'page-header__wrapper--js-enabled' элементу page-header-wrapper
-  // - класс 'main-nav__toggle--js-enabled' элементу nav-btn (показать гамбургер)
+  // - класс 'page-header__toggle--js-enabled' элементу nav-btn (показать гамбургер)
   //
   const listItems = document.getElementsByClassName("site-list__item");
   for (const listItem of listItems) {
@@ -16,29 +16,32 @@ document.addEventListener('DOMContentLoaded', function (event) {
   document.getElementById("page-header").classList.add('page-header--js-enabled');
   document.getElementById("main-nav").classList.add('main-nav__list--js-enabled');
   document.getElementById("page-header-wrapper").classList.add('page-header__wrapper--js-enabled');
-  document.getElementById("nav-btn").classList.add('main-nav__toggle--js-enabled');
+  document.getElementById("nav-btn").classList.add('page-header__toggle--js-enabled');
 
   //
   // Спрятать открытое меню
   //
   document.getElementById("main-nav").classList.remove('main-nav__list--open');
 
-
+  //
+  // NOTE! Чтобы открыть меню, добавить эти классы:
+  // - класс 'page-header__wrapper--bg' элементу page-header-wrapper
+  // - класс 'main-nav__list--open' элементу main-nav
+  // - класс 'page-header__toggle--close' элементу nav-btn
+  //
+  // Соответственно, закрыть меню - убрать классы
+  //
   var isOpen = false;
-  var button = document.getElementById('nav-btn');
-  var headerWrapBlock = document.getElementById('page-header-wrapper');
-  var navBlock = document.getElementById('main-nav');
-
-  button.addEventListener('click', function (e) {
+  document.getElementById('nav-btn').addEventListener('click', function (e) {
     isOpen = !isOpen;
     if (isOpen) {
-      headerWrapBlock.classList.add('page-header__wrapper--bg');
-      navBlock.classList.add('main-nav__list--open');
-      button.classList.add('main-nav__toggle--close');
+      document.getElementById('page-header-wrapper').classList.add('page-header__wrapper--bg');
+      document.getElementById('main-nav').classList.add('main-nav__list--open');
+      document.getElementById('nav-btn').classList.add('page-header__toggle--close');
     } else {
-      headerWrapBlock.classList.remove('page-header__wrapper--bg');
-      navBlock.classList.remove('main-nav__list--open');
-      button.classList.remove('main-nav__toggle--close');
+      document.getElementById('page-header-wrapper').classList.remove('page-header__wrapper--bg');
+      document.getElementById('main-nav').classList.remove('main-nav__list--open');
+      document.getElementById('nav-btn').classList.remove('page-header__toggle--close');
     }
   }, false);
 
